061e929e0bfbff549ad269ea121d2040a5541a01
AlexMoreira01 <77466610+AlexMoreira01@users.noreply.github.com>
2021-06-14 16:17:02 -0300

    Add files via upload

---
+++ Parte Alex/conexao.py
@@ -0,0 +1,79 @@
+import sqlite3
+
+class Conexao:
+    
+    def conectar(self):
+        conexao = None 
+        db_path = 'BancoApp.db'
+        try:
+            conexao = sqlite3.connect(db_path)
+        except sqlite3.DatabaseError as err:
+            print(f"Erro ao conectar o banco de dados {db_path}.")
+
+        return conexao
+
+    def createTableAtaReuniao(self,conexao,cursor):
+        sql = """ CREATE TABLE IF NOT EXISTS ata_reuniao (
+                    ata_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    ata_reuni_id INTEGER,
+                    ata_assunto TEXT,
+                    reuniao_id INTEGER,
+                    FOREIGN KEY (reuniao_id) REFERENCES reuniao (reuni_id));"""
+        cursor.execute(sql)
+        conexao.commit()
+
+    def createTablePresenca(self,conexao,cursor):
+        cursor.execute('DROP TABLE IF EXISTS ata_presenca')
+        
+        sql = """ CREATE TABLE IF NOT EXISTS ata_presenca( 
+                    pres_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    pres_cond_id INTEGER,
+                    pres_reuniao_id INTEGER,
+                    pres_ata_id INTEGER,
+                    presenca int,
+                    FOREIGN KEY (pres_ata_id) REFERENCES ata_reuniao (ata_id)
+                    FOREIGN KEY (pres_cond_id) REFERENCES condominos (id),
+                    FOREIGN KEY (pres_reuniao_id) REFERENCES reuniao (reuni_id));"""
+        cursor.execute(sql)
+        conexao.commit()
+        #pres_ata_id INTEGER,
+
+        #FOREIGN KEY (pres_ata_id) REFERENCES ata_reuniao (ata_id),
+
+    def createTableCondominos(self,conexao,cursor):
+        sql = """create table if not exists condominos (
+				    id integer primary key autoincrement ,
+				    cond_apartamento int,
+				    cond_cpf varchar(14),
+				    cond_nome varchar(255),
+				    cond_tppessoa varchar(20),
+				    cond_rg varchar(20),
+				    cond_nascimento date,
+				    cond_email varchar(50),
+				    cond_telresidencial varchar(11),
+				    cond_telcomercial varchar(11),
+				    cond_telcelular varchar(11),
+				    cond_adimplente bit,
+				    cond_data_cadastro datetime);"""
+        cursor.execute(sql)
+        conexao.commit()
+
+    def createTableReuniao(self,conexao,cursor):
+        sql = """ CREATE TABLE IF NOT EXISTS reuniao (
+                reuni_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                reuni_data DATETIME,
+                reuni_tema TEXT,
+                reuni_local TEXT);  """
+        cursor.execute(sql)
+        conexao.commit()
+        
+
+    def createTables(self):
+        conexao = self.conectar() 
+        cursor = conexao.cursor()
+
+        self.createTableAtaReuniao(conexao, cursor)
+        self.createTablePresenca(conexao,cursor)
+        self.createTableCondominos(conexao, cursor)
+        self.createTableReuniao(conexao,cursor)
+
